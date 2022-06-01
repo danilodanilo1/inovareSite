6687a2f7868ed99342779b5a1312258ac650709a
danilo.nascimento <danilo.nascimento@opah.com.brA>
2022-06-01 19:35:56 -0300

    sitemap

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ import Head from "next/head";
 import { FloatWhats, Linkao } from "../src/components/NavBar/NavBarElements";
 import logoWhats from "../src/components/images/img/logowhats.svg";
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -39,52 +40,68 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <Head>
-          <title>Inovare Redes</title>
-          <meta
-            name="description"
-            content="Inovare Redes de proteção para sacadas, janelas, piscinas, escadas e onde mais você quiser! (11)9 5268-8682 orçamento grátis."
-          />
-          <meta
-            property="og:title"
-            content="Redes de proteção para sacadas, janelas, piscinas, escadas e onde mais você quiser!"
-          />
-          <meta
-            property="og:description"
-            content="INOVARE - (11)9 5268-8682 orçamento grátis."
-          />
-          <meta
-            property="og:url"
-            content="https://www.inovareredesdeprotecao.com.br/"
-          />
-          <meta property="og:type" content="website"></meta>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta
-            name="google-site-verification"
-            content="kMJ2VUUTMm2TdYmxpFy_DdCcanD4zkR_S4WW65TRj-U"
-          />
-          <meta property="og:site_name" content="Inovare redes de Proteção" />
-          <meta
-            property="og:image"
-            content="https://inovareredesdeprotecao.com.br/src/components/images/img/logoInovare.svg"
-          />
-          <meta property="og:type" content="website" />
-        </Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-229986521-2"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'UA-229986521-2');
-        </script>
-      </div>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Head>
+        <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WCNJ77Z');`}</Script>
+        <title>Inovare Redes</title>
+        <meta
+          name="description"
+          content="Inovare Redes de proteção para sacadas, janelas, piscinas, escadas e onde mais você quiser! (11)9 5268-8682 orçamento grátis."
+        />
+        <meta
+          property="og:title"
+          content="Redes de proteção para sacadas, janelas, piscinas, escadas e onde mais você quiser!"
+        />
+        <meta
+          property="og:description"
+          content="INOVARE - (11)9 5268-8682 orçamento grátis."
+        />
+        <meta
+          property="og:url"
+          content="https://www.inovareredesdeprotecao.com.br/"
+        />
+        <meta property="og:type" content="website"></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="google-site-verification"
+          content="kMJ2VUUTMm2TdYmxpFy_DdCcanD4zkR_S4WW65TRj-U"
+        />
+        <meta property="og:site_name" content="Inovare redes de Proteção" />
+        <meta
+          property="og:image"
+          content="https://inovareredesdeprotecao.com.br/src/components/images/img/logoInovare.svg"
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+      <Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WCNJ77Z"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+      </Script>
       <AlertProvider template={AlertTemplate} {...options}>
         <FloatWhats>
           <Linkao
