108d0727bc83512e70baf15d4c3fa5d9d65ebd5f
danilo.nascimento <danilo.nascimento@opah.com.brA>
2022-08-29 18:08:26 -0300

    adicionando script

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,8 @@ export default function Home() {
 
       <Head>
         <title>Inovare Redes</title>
-        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WBRMKKR');`}}></Script>
+        <script strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WBRMKKR');`}}></script>
         {/* // <!-- End Google Tag Manager --> */}
 
         {/* <!-- Google Tag Manager (noscript) --> */}
@@ -54,13 +54,13 @@ export default function Home() {
 
         {/* <!-- Global site tag (gtag.js) - Google Ads: 10876347005 --> */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10876347005"></script>
-        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
+        <script strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'AW-10876347005');
         `}}>
-        </Script>
+        </script>
       </Head>
       <AlertProvider template={AlertTemplate} {...options}>
         <FloatWhats>
